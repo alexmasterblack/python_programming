b530f9aab893659d47cacf20997dd3ff2413e9b0
master <58977579+alexmasterblack@users.noreply.github.com>
2020-03-09 14:30:41 +1000

    Update bowl +.py

--- bowl +.py
+++ bowl +.py
@@ -1,9 +1,9 @@
-n = [int(i) for i in input().split()]
-bowls = [str(i) for i in range(1, n[0] + 1)]
+n, k = input().split()
+bowls = [str(i) for i in range(1, int(n)+1)]
 
-for i in range (1, n[1] + 1):
-    k = [int(j) for j in input().split()]
-    shoot = [str(j) for j in range(k[0], k[1] + 1)]
+for i in range (int(k)):
+    k_1, k_2 = input().split()
+    shoot = [str(j) for j in range(int(k_1), int(k_2) + 1)]
     for j in range(len(bowls)):
         for l in range(len(shoot)):
             if shoot[l] == bowls[j]:
