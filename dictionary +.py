58ec22e64f8a968bcea69ee4c70b4a8407fcfe13
master <58977579+alexmasterblack@users.noreply.github.com>
2020-04-06 00:51:35 +1000

    Add files via upload

---
+++ dictionary +.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+string = {}
+for i in range(n): 
+    word, *transl = input().replace(',', '').split()
+    del transl[0]
+    for j in transl:
+        if j not in string.keys():
+            string[j] = []
+        string[j].append(word)
+
+
+print(len(list(string.keys())))
+
+sorted_string = sorted(string.items())
+
+for i, j in sorted_string:
+    if type(j) == list:
+        elem = str(j).replace('[', '').replace(']', '').replace('\'', '')
+        print(i, '-', elem)
+    else:
+        print(i, '-', j)
